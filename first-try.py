5677c4a38c12a42bf2964c7c45321abfbe57dd21
soufianebr <berkanesoufiane@gmail.com>
2018-11-05 22:58:17 +0100

    reduce layer

--- first-try.py
+++ first-try.py
@@ -40,15 +40,6 @@ model.add(Conv2D(32, kernel_size=(3, 3), activation = 'relu', input_shape = inpu
 model.add(Conv2D(64, (3, 3), activation = 'relu'))
 
 
-model.add(Conv2D(64, kernel_size=(3, 3), activation = 'relu', input_shape = input_shape))
-model.add(Conv2D(64, (3, 3), activation = 'relu'))
-
-
-model.add(Conv2D(64, kernel_size=(3, 3), activation = 'relu', input_shape = input_shape))
-model.add(Conv2D(64, (3, 3), activation = 'relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-
 
 model.add(Dropout(0.25))
 model.add(Flatten())
